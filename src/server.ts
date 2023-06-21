0fcc7097781a756f991401e9c18b67c642f66109
huy1999dnbk <minhhuy99dn@gmail.com>
2023-06-21 10:28:33 +0700

    setup express 1

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,9 @@ import 'dotenv/config'
 
 const PORT = process.env.PORT || 3000
 
-
 const app = express()
-var a = 5
-
-app.listen(PORT,() => {
-	         console.log('Server is running at', PORT)
+const a = 5
+const b = 6
+app.listen(PORT, () => {
+	console.log('Server is running at', PORT)
 })
-
-
